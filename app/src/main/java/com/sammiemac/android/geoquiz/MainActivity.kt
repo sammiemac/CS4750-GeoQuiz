ee9f8708a7c842d7a169a86a35b92dc243189fcc
Sam M <snv.macaraniag@gmail.com>
2022-09-06 14:19:51 -0700

    chp3-pg73
    Finished up to Chp. 3, Pg. 73 of book
    - added arrow drawables
    - finished chp 2 and challenges
    - rotation

--- app/src/main/java/com/sammiemac/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/sammiemac/android/geoquiz/MainActivity.kt
@@ -2,16 +2,22 @@ package com.sammiemac.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    //Chp. 2 Challenge: Add a Previous Button
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -27,16 +33,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
+        nextButton = findViewById(R.id.next_image_button)
+        //Chp. 2 Challenge: Add a Previous Button
+        prevButton = findViewById(R.id.prev_image_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-            
+
         }
 
         falseButton.setOnClickListener { view: View ->
@@ -48,8 +57,49 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        //Chp. 2 Challenge: Add a Previous Button
+        prevButton.setOnClickListener { view: View ->
+            if (currentIndex == 0) {
+                currentIndex = questionBank.size - 1
+            } else {
+                currentIndex = (currentIndex - 1) % questionBank.size
+            }
+            updateQuestion()
+        }
+
+        //Chp. 2 Challenge: Add Listener to TextView
+        questionTextView.setOnClickListener { view : View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
